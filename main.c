6b7f69f791097091bdd592d2a3d47852b175df3b
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-02-25 19:41:18 -0500

    Refactor button creation into separate function for improved code organization

--- main.c
+++ main.c
@@ -23,6 +23,20 @@ void on_login_clicked(GtkWidget *widget, gpointer data) {
  * @param user_data A pointer to user data, which is cast to a char* and printed
  * to prevent clang-tidy errors.
  */
+void create_buttons(GtkWidget *window) {
+  GtkWidget *create_account_button =
+      gtk_button_new_with_label("Create Account");
+  GtkWidget *login_button = gtk_button_new_with_label("Login");
+
+  g_signal_connect(create_account_button, "clicked",
+                   G_CALLBACK(on_create_account_clicked), window);
+  g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_clicked),
+                   window);
+
+  gtk_container_add(GTK_CONTAINER(window), create_account_button);
+  gtk_container_add(GTK_CONTAINER(window), login_button);
+}
+
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Window");
@@ -35,21 +49,10 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_window_set_default_size(GTK_WINDOW(window), screen_width * 0.25,
                               screen_height * 0.25);
 
-  GtkWidget *create_account_button =
-      gtk_button_new_with_label("Create Account");
-  GtkWidget *login_button = gtk_button_new_with_label("Login");
-
-  g_signal_connect(create_account_button, "clicked",
-                   G_CALLBACK(on_create_account_clicked), window);
-  g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_clicked),
-                   window);
-
-  gtk_container_add(GTK_CONTAINER(window), create_account_button);
-  gtk_container_add(GTK_CONTAINER(window), login_button);
+  create_buttons(window);
 
   gtk_widget_show_all(window);
-  printf("%s",
-         (char *)user_data); // This is here to prevent clang-tidy errors
+  printf("%s", (char *)user_data); // This is here to prevent clang-tidy errors
 }
 
 int main(int argc, char **argv) {
