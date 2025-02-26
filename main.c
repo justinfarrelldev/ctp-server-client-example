2724b0a01a685be2375a1385a32ffd029ae92a28
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-02-25 19:26:59 -0500

    Adjust default window size to be 25% of the primary monitor's work area

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Window");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  GdkRectangle workarea = {0};
+  gdk_monitor_get_workarea(
+      gdk_display_get_primary_monitor(gdk_display_get_default()), &workarea);
+
+  gint screen_width = workarea.width;
+  gint screen_height = workarea.height;
+  gtk_window_set_default_size(GTK_WINDOW(window), screen_width * 0.25,
+                              screen_height * 0.25);
   gtk_widget_show_all(window);
   printf("%s",
          (char *)user_data); // This is here to prevent clang-tidy errors
