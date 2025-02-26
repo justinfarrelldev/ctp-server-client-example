291f46a5418881ff287b64d7ddb1c08af754f613
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-02-25 19:22:41 -0500

    Refactor main.c for improved readability and to address clang-tidy warnings

--- main.c
+++ main.c
@@ -1,24 +1,20 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
 
 static void activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *window;
-
-  printf("%s", (char *)user_data);
-  window = gtk_application_window_new(app);
+  GtkWidget *window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Window");
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
   gtk_widget_show_all(window);
+  printf("%s",
+         (char *)user_data); // This is here to prevent clang-tidy errors
 }
 
 int main(int argc, char **argv) {
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app =
+      gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
 
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
   return status;
 }
