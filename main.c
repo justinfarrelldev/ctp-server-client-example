4ef4e86e7288c04e458102292f0be145c642977b
Justin Farrell <justinfarrellwebdev@gmail.com>
2025-02-25 19:20:33 -0500

    Implement GTK application structure in main.c

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello, C11 with Clang!\n");
-  return 0;
+static void activate(GtkApplication *app, gpointer user_data) {
+  GtkWidget *window;
+
+  printf("%s", (char *)user_data);
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "Window");
+  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
 }
